099f6d29ab8f674e5b0e69263ce6da9f332c7b3a
Borna Hemmaty <Borna1103@msn.com>
2023-02-24 17:09:08 -0800

    yes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import{AiFillLinkedin, AiFillGithub, AiFillCloud} from 'react-icons/ai'
 import Icon from '/images/icon.png';
 import Image from 'next/image'
@@ -17,26 +18,26 @@ export default function Home() {
         <section className='min-h-screen'>
           <nav className="flex flex-wrap justify-between bg-slate-600 px-10 w-full py-8 cursor-pointer">
               <ul className='flex flex-wrap items-center'>
-                <a href="/"><AiFillCloud className='text-4xl text-teal-400 transition duration-200 ease-in-out hover:text-emerald-400 transform hover:scale-110'/></a>
+                <Link href="/"><AiFillCloud className='text-4xl text-teal-400 transition duration-200 ease-in-out hover:text-emerald-400 transform hover:scale-110'/></Link>
                 <li className='ml-10 transition duration-200 ease-in-out text-white hover:text-emerald-400 transform hover:scale-110'>
-                  <a href="/">Home</a>
+                  <Link href="/">Home</Link>
                 </li>
                 <li className='ml-10 transition duration-200 ease-in-out text-white hover:text-emerald-400 transform hover:scale-110'>
-                  <a href='skills'>Skills</a>
+                  <Link href='skills'>Skills</Link>
                 </li>
                 <li className='ml-10 transition duration-200 ease-in-out text-white hover:text-emerald-400 transform hover:scale-110'>
-                  <a href="projects">Projects</a>
+                  <Link href="projects">Projects</Link>
                 </li>
               </ul>
               <ul className="flex items-center">
                 <li className='text-4xl flex gap-10 text-white ml-10 content-end'>
-                  <a href="https://github.com/Borna1103" target="_blank"><AiFillGithub  href="#" className="transition duration-200 ease-in-out text-white hover:text-black transform hover:scale-110"/></a>
-                  <a href="https://www.linkedin.com/in/borna-hemmaty/" target="_blank"><AiFillLinkedin  href="google.com" className='transition duration-200 ease-in-out text-white hover:text-blue-500 transform hover:scale-110'/></a>
+                  <Link href="https://github.com/Borna1103" target="_blank"><AiFillGithub  href="#" className="transition duration-200 ease-in-out text-white hover:text-black transform hover:scale-110"/></Link>
+                  <Link href="https://www.linkedin.com/in/borna-hemmaty/" target="_blank"><AiFillLinkedin  href="google.com" className='transition duration-200 ease-in-out text-white hover:text-blue-500 transform hover:scale-110'/></Link>
                 </li>
                 <li>
-                  <a className=" transition duration-200 ease-in-out bg-gradient-to-r from-teal-600 to-emerald-400 text-white px-4 py-2 rounded ml-10 transform hover:scale-110 hover:text-gray-500" href='https://docs.google.com/document/d/1KWuzWsK7h3EPdu6ejwP-RgXp1NCmHELEJt0fr2j9-jc/edit?usp=sharing' target="_blank">
+                  <Link className=" transition duration-200 ease-in-out bg-gradient-to-r from-teal-600 to-emerald-400 text-white px-4 py-2 rounded ml-10 transform hover:scale-110 hover:text-gray-500" href='https://docs.google.com/document/d/1KWuzWsK7h3EPdu6ejwP-RgXp1NCmHELEJt0fr2j9-jc/edit?usp=sharing' target="_blank">
                     Resume
-                  </a>
+                  </Link>
                 </li>
               </ul>
           </nav>
