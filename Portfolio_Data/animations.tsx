ed45574bb3ef4f736bf9bfc9d489b73f31106e9f
Borna <Borna1103@msn.com>
2023-10-01 16:02:39 -0700

    deletion

--- Portfolio_Data/animations.tsx
+++ Portfolio_Data/animations.tsx
@@ -30,16 +30,3 @@ export function projectdots() {
         
     }
 
-export function scrollup() {
-    const scrollToTop = () => {
-        scroll.scrollToTop({ smooth: true });
-    };
-
-    return(
-    <button className={`fixed bottom-4 right-4 bg-gray-800 text-white p-2 rounded-full transition-opacity ${visible ? 'opacity-100' : 'opacity-0'}`} onClick={scrollToTop}>
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
-        </svg>
-    </button>
-    )
-}
