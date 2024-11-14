35abd04f134abc7687241052555e9316639397e5
Borna <Borna1103@msn.com>
2024-11-14 14:58:49 -0800

    Light mode fixes

--- pages/myClasses.tsx
+++ pages/myClasses.tsx
@@ -5,8 +5,8 @@ export default function Classes() {
         <div className='rounded-2xl borderp-6 border-zinc-100 border-zinc-700/40 bg-gray-800 p-6 pl-8 overflow-y-auto no-scrollbar classes'>
             <h2 className="flex text-base font-semibold tracking-tight text-white">
                 <svg viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" className="h-6 w-6 flex-none">
-                    <path d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z" className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"></path>
-                    <path d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5" className="stroke-zinc-400 dark:stroke-zinc-500"></path>
+                    <path d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z" className="fill-zinc-100/10 stroke-zinc-500"></path>
+                    <path d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5" className="stroke-zinc-500"></path>
                 </svg>
 
                 <span className="ml-3">Classes</span>
@@ -17,9 +17,9 @@ export default function Classes() {
                 <li key={course.id} className="flex gap-4">
                    
                     <div className="flex flex-auto flex-wrap gap-x-2">
-                        <h2 className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">{course.name}</h2>
-                        <h2 className="text-xs text-zinc-500 dark:text-zinc-400">{course.skills}</h2>
-                        <time className="ml-auto text-xs text-zinc-400 dark:text-zinc-500">{course.time}</time>
+                        <h2 className="w-full flex-none text-sm font-medium text-zinc-100">{course.name}</h2>
+                        <h2 className="text-xs text-zinc-400">{course.skills}</h2>
+                        <time className="ml-auto text-xs text-zinc-500">{course.time}</time>
                     </div>
                 </li>
                ))} 
--- pages/myFooter.tsx
+++ pages/myFooter.tsx
@@ -4,9 +4,9 @@ export default function Footer() {
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="https://borna1103.github.io/Borna/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Borna Hemmaty</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Borna Hemmaty</span>
                     </a>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium  sm:mb-0 text-gray-400">
                         <li>
                             <a href="#Home" className="hover:underline me-4 md:me-6">Home</a>
                         </li>
@@ -21,8 +21,8 @@ export default function Footer() {
                         </li>
                     </ul>
                 </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://borna1103.github.io/Borna/" className="hover:underline">BornaHemmaty™</a>. All Rights Reserved.</span>
+                <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
+                <span className="block text-sm sm:text-center text-gray-400">© 2023 <a href="https://borna1103.github.io/Borna/" className="hover:underline">BornaHemmaty™</a>. All Rights Reserved.</span>
             </div>
         </footer>
     )
